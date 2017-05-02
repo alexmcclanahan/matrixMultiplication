b345a96b8726975e7889aab78a82b913250eff0e
Alex McClanahan <alexmcclanahan94@gmail.com>
2017-05-02 14:26:37 -0400

    Update naive_solution.py
    Updated, with less code and optimized to handle any two matrices, as long as Aik and Bkj are our matrices.

--- naive_solution.py
+++ naive_solution.py
@@ -1,57 +1,24 @@
-#a matrix multiplication algorithm
-import randMatrix
-
-def initC(n):
+#an attempt at less robust naive (n^3) solution of matrix multiplication
+def mult(A, B):
+    # colA=rowB is the condition of mat. mult.
     C = []
-    for j in range(0, n):
-        C.insert(j, [0]*n)
-    return C
-
-def matrixM(A, B):
-    #len delivers the number of rows in a matrix
-    n = len(A)
-    C = initC(n)
-    if len(A) == len(B):
-        if len(A[0]) == len(B):
-            # case where we have two arrays of n x n; must make sure columns of A = rows of B
-            for i in range(0, n):
-                for j in range(0, n):
-                    for k in range(0, n):
-                        C[i][j] += A[i][k]*B[k][j]
-        else:
-            print "Cross product matrix is not defined. The first matrix must have the same number of columns as the second matrix has rows."
-            return
-    if len(A[0]) == len(B):
-        # case where we have two arrays which don't have the same dimensions
-        if len(A) < len(B):
-            # subcase where the first array is wider and less skinny than the second, B array
-            for i in range(0, n):
-                for j in range(0, n):
-                    for k in range(0, n+1):
-                        C[i][j] += A[i][k] * B[k][j]
-        if len(A) > len(B):
-            #subcase where the first array is taller and skinner than the B array
-            for i in range(0, n):
-                for j in range(0, n):
-                    for k in range(0, n-1):
-                        C[i][j] += A[i][k] * B[k][j]
-        print C
-    else:
-        print "Cross product matrix is not defined. The first matrix must have the same number of columns as the second matrix has rows."
-        return
-print '-----------------------------------------------------'
-print '-----------------------------------------------------'
-print "First (x) random generated matrix is:", randMatrix.x
-print "Second (y) random generated matrix is: ", randMatrix.y
-
-matrixM(randMatrix.x, randMatrix.y)
-
-print '-----------------------------------------------------'
-print '-----------------------------------------------------'
-
-#extinct matrixmult
-#A = [[2, 1, 3], [2, 3, 1]]
-#B = [[2, 1], [132, 2], [1, 0]]
-#E = [[5, 10], [1, 2], [2, 3]]
-#F = [[2, 3], [6, 7]]
-#H = [[2, 2], [1, 1]]
+    for a in range(0, len(A)):
+        C.append([0] * len(B[0]))
+    for i in range(0, len(A)):
+        for j in range(0, len(B[0])):
+            C[i][j] = 0
+            for k in range(0, len(B)):
+                C[i][j] += A[i][k]*B[k][j]
+    print C
+A = [[2, 1, 3], [2, 3, 1]]
+B = [[2, 1], [132, 2], [1, 0]]
+E = [[5, 10], [1, 2], [2, 3]]
+F = [[2, 3], [6, 7]]
+H = [[2, 2], [1, 1]]
+Q = [[1, 2104], [1, 1416], [1, 1534], [1, 852]]
+QQ = [[-40], [0.25]]
+mult(A,E)
+#final conclusion: in order to naively calculate matrix multiplication with two nxn matricies,
+#one simply has to loop k in range of original n. I had to loop k up to a second variable, v,
+#in order to account for the cases where we have two matrices with unequal dimensions. Therefore,
+#A and B inputs into mult(A, B) can now be any two matrices AS LONG AS columns of A = rows of B.
